bffec661b40b45a9cc10e18360982337a3c1fdc7
H. Vetinari <h.vetinari@gmx.com>
2026-03-05 15:35:27 +1100

    remove tabs in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,11 +32,11 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
 fi
 
 cmake \
-	${CMAKE_ARGS} \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_MODULE_PATH=${PREFIX}/lib/cmake/llvm \
-	-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=BOTH \
-	..
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_MODULE_PATH=${PREFIX}/lib/cmake/llvm \
+  -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=BOTH \
+  ..
 
 make -j${CPU_COUNT}
 make install
